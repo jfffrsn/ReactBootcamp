cfa15de3da9a6b8794911a6673597bf197ea1c2d
jfffrsn <jfffrsn@users.noreply.github.com>
2019-09-10 16:39:10 -0400

    initial

---
+++ roll-dice/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RollDice from './RollDice';
+
+
+function App() {
+  return (
+    <div className="App1">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
