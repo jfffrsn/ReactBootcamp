9dc655fc7b7d7767761e423405157ccb9d609bea
jfffrsn <jfffrsn@users.noreply.github.com>
2019-09-10 16:33:10 -0400

    initial

---
+++ color-box-maker/src/BoxList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import NewBoxForm from './NewBoxForm';
+
+
+class BoxList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            boxes: []
+        };
+        this.create = this.create.bind(this);
+    }
+
+    remove(id) {
+        this.setState ({
+            boxes: this.state.boxes.filter(box => box.id !== id)
+        })
+    }
+
+    create(newBox) {
+        this.setState ({
+            boxes: [...this.state.boxes, newBox]
+        });
+    }
+    render() {
+        const boxes = this.state.boxes.map(box => (
+            <Box
+            key={box.id} 
+            id={box.id}
+            width={box.width} 
+            height={box.height} 
+            color={box.color}
+            removeBox={() => this.remove(box.id)}
+            />
+        ));
+        return (
+            <div>
+                <h1>Color Box Maker thingy</h1>
+                {boxes}
+                <NewBoxForm createBox={this.create}/>
+            </div> 
+        );
+    }
+}
+
+export default BoxList;
